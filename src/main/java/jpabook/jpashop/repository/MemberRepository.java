cb3f8978edf3e59eba6dac0100911b0caa5ea8e4
layerdst <bccfcc@hanmail.net>
2022-05-12 18:51:52 +0900

    SPRING JPA

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository  extends JpaRepository<Member, Long> {
+
+}
---
+++ src/test/java/jpabook/jpashop/contoller/HelloControllerTest.java
@@ -0,0 +1,7 @@
+package jpabook.jpashop.contoller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+}
